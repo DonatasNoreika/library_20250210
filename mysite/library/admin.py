5198ac922332ff3fe78054a2ddc91929374c1e94
DonatasNoreika <donoras@gmail.com>
2025-02-11 09:37:19 +0200

    2: nustatytas knygų kopijų atvaizdavimo sąrašas

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -4,8 +4,12 @@ from .models import Author, Book, Genre, BookInstance
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'isbn', 'display_genre']
 
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'due_back', 'status']
+    list_filter = ['due_back', 'status']
+
 # Register your models here.
 admin.site.register(Author)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+admin.site.register(BookInstance, BookInstanceAdmin)
