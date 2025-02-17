243bdb0519c221bdd620d137f51c69394d27d4c3
DonatasNoreika <donoras@gmail.com>
2025-02-17 09:24:15 +0200

    5: Supuslapiuotas knygų sąrašas

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -34,6 +34,7 @@ class BookListView(generic.ListView):
     model = Book
     template_name = "books.html"
     context_object_name = "books"
+    paginate_by = 5
 
 
 class BookDetailView(generic.DetailView):
