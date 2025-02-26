562ca726926c8886ac470211490ea2f6252017e9
DonatasNoreika <donoras@gmail.com>
2025-02-26 10:15:56 +0200

    11: +Įdėtas mygtukas kalbos keitimui

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('library/', include("library.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', RedirectView.as_view(url='library/', permanent=True)),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
